cc236344f5e1619ae3c0317f3da5af80ed3db9d0
elkin <filasachi@gmail.com>
2024-08-14 18:43:19 -0500

    se crea la tabla unidad_medida y el elemento 'sin unidad'

--- factubarrio.sql
+++ factubarrio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-08-2024 a las 01:30:02
+-- Tiempo de generación: 15-08-2024 a las 01:41:50
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -59,6 +59,25 @@ CREATE TABLE `direccion` (
   `pais` varchar(50) NOT NULL DEFAULT 'desconocido'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `unidad_medida`
+--
+
+CREATE TABLE `unidad_medida` (
+  `idunidad_medida` int(11) NOT NULL,
+  `unidad_medida` varchar(20) NOT NULL,
+  `unidad_padre_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `unidad_medida`
+--
+
+INSERT INTO `unidad_medida` (`idunidad_medida`, `unidad_medida`, `unidad_padre_id`) VALUES
+(1, 'Sin Unidad', NULL);
+
 --
 -- Índices para tablas volcadas
 --
@@ -80,6 +99,14 @@ ALTER TABLE `direccion`
   ADD KEY `idx_pais` (`pais`);
 
 --
+-- Indices de la tabla `unidad_medida`
+--
+ALTER TABLE `unidad_medida`
+  ADD PRIMARY KEY (`idunidad_medida`),
+  ADD UNIQUE KEY `unidad_medida` (`unidad_medida`),
+  ADD KEY `unidad_padre_id` (`unidad_padre_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -94,6 +121,22 @@ ALTER TABLE `categoria`
 --
 ALTER TABLE `direccion`
   MODIFY `iddireccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `unidad_medida`
+--
+ALTER TABLE `unidad_medida`
+  MODIFY `idunidad_medida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `unidad_medida`
+--
+ALTER TABLE `unidad_medida`
+  ADD CONSTRAINT `unidad_medida_ibfk_1` FOREIGN KEY (`unidad_padre_id`) REFERENCES `unidad_medida` (`idunidad_medida`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
